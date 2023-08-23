bf0ae32da7b871934ca18a0eeff581fc72a89dfe
Aria1228 <ari16a28@gmail.com>
2023-08-23 14:03:12 +0800

    商品功能 - Refactor category，使用 Enum 類型

--- src/main/java/com/aria/springbootmall/rowmapper/ProductRowMapper.java
+++ src/main/java/com/aria/springbootmall/rowmapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.aria.springbootmall.rowmapper;
 
+import com.aria.springbootmall.constant.ProductCategory;
 import com.aria.springbootmall.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -14,7 +15,23 @@ public class ProductRowMapper implements RowMapper<Product> {
 
         product.setProductId(resultSet.getInt("product_id"));
         product.setProductName(resultSet.getString("product_name"));
-        product.setCategory(resultSet.getString("category"));
+
+        // String 轉 Enum 寫法
+        // 寫一 categoryStr 的 String 出來，接住 resultSet 中取出的資料庫的值
+        String categoryStr = resultSet.getString("category");
+
+        // 將字串轉換成 Enum 類型
+        // 根據傳進去的字串的值，找尋對應中的 ProductCategory 中的固定值，存放在 category 的變數中
+        ProductCategory category = ProductCategory.valueOf(categoryStr);
+
+        // 將 category 變數傳入 product 的 set 方法內
+        product.setCategory(category);
+
+        /* 可將上方程式改寫為一行：
+        *  product.setCategory(ProductCategory.valueOf(resultSet.getString("category")));
+        *  直接將資料庫中取出來的字串，轉換成 category 的 Enum 值，傳入到 set 方法內做設定*/
+
+
         product.setImageUrl(resultSet.getString("image_url"));
         product.setPrice(resultSet.getInt("price"));
         product.setStock(resultSet.getInt("stock"));
